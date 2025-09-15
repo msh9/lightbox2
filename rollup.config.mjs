996e391dc1347b9847dde44bd6cca4983a379b62
Michael Hughes <michael@mihughes.com>
2025-09-15 15:48:41 -0600

    Build tool updates (#2)
    - Update dev toolchain 
    - add linting across JS and CSS
    - add gh action linting
    - add in-repo example / test images
    - change main js file to just export the lightbox object and rely on
    rollup for iife packaging, etc

---
+++ rollup.config.mjs
@@ -0,0 +1,52 @@
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default [
+  {
+    input: 'src/js/lightbox.js',
+    output: {
+      file: 'dist/js/lightbox-jquery.js',
+      format: 'umd',
+      name: 'lightbox'
+    },
+    plugins: [resolve(), commonjs()]
+  },
+  {
+    input: 'src/js/lightbox.js',
+    output: {
+      file: 'dist/js/lightbox-jquery.min.js',
+      format: 'umd',
+      name: 'lightbox'
+    },
+    plugins: [resolve(), commonjs(), terser()]
+  },
+  {
+    input: 'src/js/lightbox.js',
+    external: ['jquery'],
+    output: {
+      file: 'dist/js/lightbox.module.js',
+      format: 'es',
+      name: 'lightbox'
+    },
+    plugins: [resolve(), commonjs()]
+  },
+  {
+    input: 'src/js/lightbox.js',
+    output: {
+      file: 'dist/js/lightbox-jquery.module.js',
+      format: 'es',
+      name: 'lightbox'
+    },
+    plugins: [resolve(), commonjs()]
+  },
+  {
+    input: 'src/js/lightbox.js',
+    output: {
+      file: 'dist/js/lightbox-jquery.module.min.js',
+      format: 'es',
+      name: 'lightbox'
+    },
+    plugins: [resolve(), commonjs(), terser()]
+  }
+];
